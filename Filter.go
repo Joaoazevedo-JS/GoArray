a68cdafdbc620721091fea6df120e970eb50414e
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-10-15 19:04:25 -0300

    feat: add filter function

---
+++ Filter.go
@@ -0,0 +1,15 @@
+package Array
+
+func (array *Array[T]) Filter(callback func(currentValue T, index int, self *Array[T]) bool) Array[T] {
+	list := New[T]()
+
+	for index, value := range array.Values {
+		add := callback(value, index, array)
+
+		if add {
+			list.Push(value)
+		}
+	}
+
+	return list
+}
