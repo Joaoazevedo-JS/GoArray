804b66e4742671bfe85483b3ddeee445090b7793
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-10-15 19:06:30 -0300

    feat: add unShift function

---
+++ Unshift.go
@@ -0,0 +1,12 @@
+package Array
+
+func (array *Array[T]) Unshift(values ...T) int {
+	list := New[T]()
+
+	list.Push(values...)
+	list.Push(array.Values...)
+
+	array.Values = list.Values
+
+	return array.Length()
+}
