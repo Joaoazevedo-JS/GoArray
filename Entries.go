b37dbc5e7825bd43d68944e207576f6d2e08483a
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-10-15 19:04:52 -0300

    feat: add entries function

---
+++ Entries.go
@@ -0,0 +1,16 @@
+package Array
+
+type Entry []interface{}
+
+func (array *Array[T]) Entries() Array[Entry] {
+	entries := New[Entry]()
+
+	array.ForEach(func(currentValue T, index int, _ *Array[T]) {
+		entries.Push(Entry{
+			index,
+			currentValue,
+		})
+	})
+
+	return entries
+}
