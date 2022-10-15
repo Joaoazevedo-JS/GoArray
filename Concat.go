546d5302a274fd7fd519d6e323071b659249962c
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-10-15 19:04:39 -0300

    feat: add concat function

---
+++ Concat.go
@@ -0,0 +1,13 @@
+package Array
+
+func (array *Array[T]) Concat(arrays ...Array[T]) Array[T] {
+	concat := New[T]()
+
+	concat.Push(array.Values...)
+
+	for _, value := range arrays {
+		concat.Push(value.Values...)
+	}
+
+	return concat
+}
