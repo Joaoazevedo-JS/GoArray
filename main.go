81f71e864e8ce52e7ae0d32cf169d75f759281c5
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-10-15 18:44:50 -0300

    feat: create Array Type and new func

---
+++ main.go
@@ -0,0 +1,15 @@
+package Array
+
+type Array[T any] struct {
+	Values []T
+}
+
+func New[T any](values ...T) Array[T] {
+	array := Array[T]{
+		Values: []T{},
+	}
+
+	array.Push(values...)
+
+	return array
+}
